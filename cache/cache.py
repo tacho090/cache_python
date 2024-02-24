452ecb61f3ff4345774f542c981d8ce607ea3d5d
Pablo Anastacio Mora Chatski <panastacio@dotdashmdp.com>
2024-02-24 17:19:51 -0600

    add logging module

--- cache/cache.py
+++ cache/cache.py
@@ -1,24 +1,31 @@
+import log
+
 class Cache:
     def __init__(self, max_size):
         self.max_size = max_size
         self.cache = {}
+        self.logger = logging.getLogger(__name__)
 
     def get(self, key):
         if key in self.cache:
+            self.logger.info(f"Get key '{key}' from cache")
             return self.cache[key]
         else:
+            self.logger.warning(f"Key '{key}' not found in cache")
             return None
 
     def set(self, key, value):
         if len(self.cache) >= self.max_size:
             self.recycle_memory()
         self.cache[key] = value
+        self.logger.info(f"Added key '{key}' to cache.")
 
     def recycle_memory(self):
         # Implement your memory recycling strategy here
         # For simplicity, let's just remove the oldest item
         oldest_key = next(iter(self.cache))
         del self.cache[oldest_key]
+        self.logger.info(f"Recycled memory by removing key '{oldest_key}' from cache")
 
 
 if __name__ == '__main__':
---
+++ log/log.py
@@ -0,0 +1,58 @@
+import logging
+
+class MyLogger:
+    def __init__(self, name, filename='example.log', filemode='w', level=logging.DEBUG):
+        
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(level)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        
+        # Create console handler
+        self.consoleHandler(level, formatter)
+
+        # Create file handler if filename is provided
+        self.fileHandler(filename, filemode, level, formatter)
+
+    def debug(self, message):
+        self.logger.debug(message)
+
+    def info(self, message):
+        self.logger.info(message)
+
+    def warning(self, message):
+        self.logger.warning(message)
+
+    def error(self, message):
+        self.logger.error(message)
+
+    def critical(self, message):
+        self.logger.critical(message)
+
+    def consoleHandler(self, level, formatter):
+        ch = logging.StreamHandler()
+        ch.setLevel(level)
+        ch.setFormatter(formatter)
+        self.logger.addHandler(ch)
+
+    def fileHandler(self, filename, filemode, level, formatter):
+        if filename:
+            fh = logging.FileHandler(filename, filemode)
+            fh.setLevel(level)
+            fh.setFormatter(formatter)
+            self.logger.addHandler(fh)
+
+
+# Example usage:
+logger_name = 'example_logger'
+log_filename = 'critical.log'
+filemode = 'w'
+level = logging.CRITICAL
+
+if __name__ == "__main__":
+    logger = MyLogger(logger_name, log_filename, filemode, level)
+
+    logger.debug("This is a debug message")
+    logger.info("This is an info message")
+    logger.warning("This is a warning message")
+    logger.error("This is an error message")
+    logger.critical("This is a critical message")

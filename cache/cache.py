54f39048694c25a95ef2d6d4279f122d53be0154
Pablo Anastacio Mora Chatski <panastacio@dotdashmdp.com>
2024-02-24 19:12:53 -0600

    Generate time logger

--- cache/cache.py
+++ cache/cache.py
@@ -1,54 +1,79 @@
 from cache_log.log import MyLogger
 import re
+import time
+from functools import wraps
 
 class Cache:
     def __init__(self, max_size):
         self.max_size = max_size
         self.cache = {}
-        self.logger = MyLogger('main_logger', 'main_logger.log')
-        self.logger.info("Start logger instance")
+        self.event_logger = MyLogger('event_logger', 'event_logger.log')
+        self.time_logger = MyLogger('time_logger', 'time_logger.log')
+        self.event_logger.info("Start event logger")
+        self.time_logger.info("Start time logger")
 
+    # Decorator to measure execution time
+    def measure_time(func):
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
+            start_time = time.time()
+            result = func(self, *args, **kwargs)
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            self.time_logger.info(
+                f"Execution time of {func.__name__}: {elapsed_time} seconds"
+            )
+            return result
+        return wrapper
+
+    @measure_time
     def get(self, key):
         if key in self.cache:
-            self.logger.info(f"Get key '{key}' from cache")
+            self.event_logger.info(f"Get key '{key}' from cache")
+            self.event_logger.info(self.cache[key])
             return self.cache[key]
         else:
-            self.logger.warning(f"Key '{key}' not found in cache")
+            self.event_logger.warning(f"Key '{key}' not found in cache")
             return None
 
+    @measure_time
     def set(self, key, value):
-        if self.regex_eval(value):
+        if self.re_eval(value):
             if len(self.cache) >= self.max_size:
                 self.recycle_memory()
             self.cache[key] = value
-            self.logger.info(f"Added key '{key}' to cache.")
+            self.event_logger.info(f"Added key '{key}' to cache.")
 
+    @measure_time
     def recycle_memory(self):
         # Implement your memory recycling strategy here
-        # For simplicity, let's just remove the oldest item
-        oldest_key = next(iter(self.cache))
+        # Remove oldest item based on date added using lambda function as filter
+        oldest_key = min(self.cache, key=lambda x: self.cache[x]['date_added'])
         del self.cache[oldest_key]
-        self.logger.info(f"Recycled memory by removing key '{oldest_key}' from cache")
+        self.event_logger.info(f"Recycled memory by removing key '{oldest_key}' from cache")
+        print(self.cache)
 
-    def regex_eval(self, value):
+    def re_eval(self, value):
         pattern = r'^[a-zA-Z0-9]+$'
-        return re.match(pattern, value)
-        
+        print(value)
+        return re.match(pattern, value["value"])        
 
 
 
 if __name__ == '__main__':
     # Example usage
     cache = Cache(3)
-    cache.set(1, "data1!")
-    cache.set(2, "data2")
-    cache.set(3, "data3")
-
-    print(cache.get(1))  # Output: data1
-    print(cache.get(2))  # Output: data2
-    print(cache.get(3))  # Output: data3
+    for i in range(3):
+        current_time = time.perf_counter()
+        cache.set(i, {
+            "date_added": current_time,
+            "value": f"data{i}"
+        })
+        cache.get(i)
 
     # Adding new data, which will trigger memory recycling
-    cache.set(4, "data4")
-
-    print(cache.get(1))  # Output: None (data1 was recycled)
+    cache.set(4, {
+        "date_added": current_time,
+        "value": "data1"
+    })
+    print(cache.get(0))  # Output: None (data1 was recycled)
